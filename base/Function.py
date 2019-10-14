f2cf3c6a782d7b21503722298f7e7d8fc6d43d68
panta <pantaoling@mytijian.com>
2019-10-14 11:33:22 +0800

    operator

--- base/Function.py
+++ base/Function.py
@@ -1,3 +1,6 @@
+import math
+
+
 def hello():
     print("hello world")
 
@@ -120,12 +123,18 @@ print(sum(2, 4))
 gCount = 0  # 全局作用域
 
 
-def outer():
+def outer(a):
     oCount = 1  # 闭包函数外作用
 
     def inner():
         iCount = 2  # 局部作用域
+        print("作用域测试", a)
+        return a
 
+    return inner()
+
+
+outer(1)
 
 # 模块（module），类（class）以及函数（def、lambda）才会引入新的作用域，
 # 其它的代码块（如 if/elif/else/、try/except、for/while等）是不会引入新的作用域的，也就是说这些语句内定义的变量，外部也可以访问
@@ -160,8 +169,6 @@ def printa(s1, s2):
 r = printa("a", "v")
 
 print(r)
-
-import math
 
 
 def quadratic_equation(a, b, c):
@@ -198,7 +205,79 @@ print(firstCharUpper('sunday'))
 print(firstCharUpper('september'))
 
 
+# 定义一个函数，有三个参数，前两个参数为数值，最后一个参数为函数，计算传入abs函数的值
+
 def add(x, y, f):
     return f(x) + f(y)
 
+
 print(add(2, 3, abs))
+import math
+
+
+def add(x, y, f):
+    return f(x) + f(y)
+
+
+print(add(25, 9, math.sqrt))
+
+
+def format_name(s):
+    L = []
+    for str1 in s:
+        newStr = str1[:1].upper() + str1[1:]
+        L.append(newStr)
+    return L
+
+
+map(format_name, ['adam', 'LISA', 'barT'])
+
+
+def is_sqr(x):
+    num = math.sqrt(x)
+    if isinstance(num, int):
+        return num
+
+
+print(filter(is_sqr, range(0, 101)))
+
+import math
+
+
+def is_sqr(x):
+    print(math.sqrt(x))
+    return math.sqrt(x) % 1 == 0
+
+
+print(filter(is_sqr, range(1, 101)))
+
+print(9.0 % 1 == 0)
+
+
+def count():
+    fs = []
+    for i in range(1, 4):
+        def f(j=i):
+            return j * j
+
+        fs.append(f)
+    return fs
+
+
+f1, f2, f3 = count()
+print(f1(), f2(), f3())
+
+f4,f5,f6 = [1,2,3]
+
+print(f4,f5,f6)
+print (sorted([1, 3, 9, 5, 0],reverse=True))
+
+import operator
+
+print(operator.le(1,3))
+print(operator.le(1,1))
+print(operator.lt(1,4))
+print(operator.ge(3,4))
+print(operator.ge(3,3))
+print(operator.gt(4,3))
+print(operator.gt(3,4))
