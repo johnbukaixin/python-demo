8fd1e1b6ce69b00c916e5edf877da31f754e5ca5
panta <pantaoling@mytijian.com>
2019-10-16 18:09:29 +0800

    class 创建实例方法

--- base/Class.py
+++ base/Class.py
@@ -32,3 +32,43 @@ L2 = sorted(L1, key=functools.cmp_to_key(cmp))
 print(L2[0].name)
 print(L2[1].name)
 print(L2[2].name)
+
+
+class Person(object):
+    count = 0
+
+    def __init__(self, name):
+        self.name = name
+        Person.count = Person.count + 1
+
+
+p1 = Person('Bob')
+print(Person.count)
+
+p2 = Person('Alice')
+print(Person.count)
+
+p3 = Person('Tim')
+print(Person.count)
+
+class Person1(object):
+
+    def __init__(self, name, score):
+        self.name = name
+        self.__score = score
+
+    def get_grade(self):
+        if self.__score >= 90:
+            return 'A-优秀'
+        if self.__score >= 60 and self.__score < 90:
+            return 'B-及格'
+        else:
+            return 'C-不及格'
+
+p4 = Person1('Bob', 90)
+p5 = Person1('Alice', 65)
+p6 = Person1('Tim', 48)
+
+print(p4.get_grade())
+print(p5.get_grade())
+print(p6.get_grade())
