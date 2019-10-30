823c4379780a7c880a6938a6e86e78c151b795a6
johnbukaixin <1151367905@qq.com>
2019-10-30 17:59:39 +0800

    minor

--- smallTopics/sensitiveWords/FindWords.py
+++ smallTopics/sensitiveWords/FindWords.py
@@ -2,6 +2,19 @@
 # CreateDate:2019/10/30
 # FileName:FindWords
 # IDE:PyCharm
+
+# 敏感词文本文件 filtered_words.txt，里面的内容为以下内容，当用户输入敏感词语时，则打印出 Freedom，否则打印出 Human Rights。
+# 北京
+# 程序员
+# 公务员
+# 领导
+# 牛比
+# 牛逼
+# 你娘
+# 你妈
+# love
+# sex
+# jiangge
 from smallTopics.ReadFileUtil import read_file
 
 
